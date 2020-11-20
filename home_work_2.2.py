6aecabdec27450e48045acc6ad3b261ff8456aa5
anuta <71727847+Tottoro932@users.noreply.github.com>
2020-11-20 19:58:27 +0300

    Update home_work_2.2.py

--- home_work_2.2.py
+++ home_work_2.2.py
@@ -10,24 +10,21 @@ def zol(a,k,E):
     E=E/10
     a1=a
     b1=a-a/4
-    d=0
-    c=0
+    d=(math.sqrt(5)-1)*(b1-a1)/2+a1
+    c=(3-math.sqrt(5))*(b1-a1)/2+a1
     while ((b1-a1)/2> E):
-        if c==0:
-            c=(3-math.sqrt(5))*(b1-a1)/2+a1
-            f_c = f(c,k)
-        if d==0:
-            d=(math.sqrt(5)-1)*(b1-a1)/2+a1
-            f_d=f(c,k)
+
+        f_c = f(c,k)
+        f_d=f(c,k)
 
         if f_c<=f_d:
             b1=d
             d=c
-            c=0
+            c=(3-math.sqrt(5))*(b1-a1)/2+a1
         else:
             a1=c
             c=d
-            d=0
+            d=(math.sqrt(5)-1)*(b1-a1)/2+a1
     return((b1+a1)/2)
 
 #градиентный спуск (2)
@@ -46,6 +43,6 @@ while abs(f(x1,y1)-f(x0,y0))>E or math.sqrt((x1-x0)**2+(y1-y0)**2)>E:
     x1=zol(x0,y0,E)
     # фиксируем 1 координату
     y1 = zol(y0,x1,E)
-print("метода касательных:",x1,y1)
+print("точка минимума:",x1,y1)
 print("количество итераций:",col_steps)
 
